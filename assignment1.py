9af11ec307dfa1961d97386df25ed299c9461408
Rishav Raj <38533962+rishavasitis@users.noreply.github.com>
2023-02-10 13:42:57 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,9 @@
+#Write a Python program to get the Fibonacci series between 0 to 50
+# Fibonacci series The Fibonacci sequence is a series of numbers ,
+# where a number is the addition of the last two numbers.
+
+a,b=0,1
+
+while b<50:
+    print(b)
+    a,b = b,a+b
